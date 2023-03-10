38a0f026b90f9085db0799fc917e73803e06806a
Ht <htcvhs971@gmail.com>
2023-03-10 11:04:51 +0100

    Mouse Events

--- JS-Ex/script.js
+++ JS-Ex/script.js
@@ -3,15 +3,7 @@ const btn1 = document.querySelector('#btn-1');
 const btn2 = document.getElementById('btn-2');
 const response = document.querySelector('p');
 //Priorité sélection: # > . > tag
-//CLICK EVENT
-questionContainer.addEventListener('click', () => {
-	//Injecter du style
-	questionContainer.style.background = 'linear-gradient(to right, #f0f2f0, #000c40)';
-	//Injecter du style sur des propriété en 2 mots -(camelCase)
-	questionContainer.style.borderRadius = 50 + '%';
-	//Injection de plusieurs style sur le le même tag => add class/remove ou toggle
-	questionContainer.classList.toggle('nom de la classe');
-});
+//CLICK EVENT----------------------------------
 //Apparition de la réponse
 btn1.addEventListener('click', () => {
 	response.classList.toggle('show-response');
@@ -20,4 +12,36 @@ btn1.addEventListener('click', () => {
 btn2.addEventListener('click', () => {
 	response.classList.toggle('show-response');
 	response.style.background = 'red';
+});
+
+//MOUSE EVENTS -------------------------
+const mousemove = document.querySelector('.mousemove');
+//MOUSEMOVE
+window.addEventListener('mousemove', (e) => {
+	// console.log(e);
+	mousemove.style.left = e.pageX + 'px';
+	mousemove.style.top = e.pageY + 'px';
+});
+//Click long = MOUSEDOWN
+window.addEventListener('mousedown', () => {
+	//Scale change => le % aussi
+	mousemove.style.transform = 'scale(2) translate(-25%, -25%)';
+});
+//Pas de Click = MOUSEUP
+window.addEventListener('mouseup', () => {
+	//Scale change => le % aussi
+	mousemove.style.transform = 'scale(1) translate(-50%, -50%)';
+	mousemove.style.border = '2px solid teal';
+});
+//MOUSEENTER
+questionContainer.addEventListener('mouseenter', () => {
+	questionContainer.style.background = 'rgba(0,0,0,0.6)';
+});
+//MOUSEOUT
+questionContainer.addEventListener('mouseout', () => {
+	questionContainer.style.background = 'blue';
+});
+//MOUSEOVER
+response.addEventListener('mouseover', () => {
+	response.style.transform = 'rotate(2deg)';
 });
