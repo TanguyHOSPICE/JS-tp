9b423e87b848b2c221dca638e4955544a73e66ef
Ht <htcvhs971@gmail.com>
2023-04-07 12:52:31 +0200

    Add json file

--- JS-API/index.js
+++ JS-API/index.js
@@ -1,10 +1,18 @@
 /**PRESENTATION Method xmlHttpRequest */
 
 //2-fonction de callback
-function reqListener() {
+/* function reqListener() {
 	console.log(this.responseText);
 }
 
 let req = new XMLHttpRequest(); //1-ou xhr
 //3-Ecouteur lors chargemment requete
 req.addEventListener('load', reqListener); // OU req.onload = reqListener;
+//recherche des données sur elts distant ici data.txt
+req.open('GET', 'data.txt', true);
+//recherche des données sur elts distant ici data.json
+//req.open('GET', 'data.json', true); //renvoie l'objet json
+//recherche des données sur elts distant ici url
+//req.open('GET', 'https://jsonplaceholder.typicode.com/posts', true); //renvoie l'objet json
+//4-envoi de la requete
+req.send(); */
