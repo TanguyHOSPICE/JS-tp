fd7c573860fb3e84e73b2a2d5fa07b5d7655ff3e
Ht <htcvhs971@gmail.com>
2023-04-11 19:37:30 +0200

    Add function to display the users

--- User-App/index.js
+++ User-App/index.js
@@ -10,10 +10,29 @@ const fetchUsers = async () => {
 		.then((res) => res.json())
 		.then((data) => (usersData = data.results)); //2bis- console.log(data.results) & 4bis-ajouter les users dans le tableau usersData =
 
-	console.log(usersData); //5bis- log usersData ac un await du fetch
+	console.log(usersData[0]); //5bis- log usersData ac un await du fetch
 };
 
-//6-fonction afficher les users
+//6-fonction afficher les users la rendre async pr afficher le map
+const displayUsers = async () => {
+	//3bis-Appeler la fonction & awwait le fait de chercher les users
+	await fetchUsers();
 
-//3bis-Appeler la fonction
-fetchUsers();
+	document.body.innerHTML = usersData
+		.map(
+			//chq tour de boucle, on recupere un user ds une card
+			(user) =>
+				`
+      <div class="card">
+        <img src="${user.picture.large}" alt="photo de ${user.name.first}" />
+        <h3>${user.name.first} ${user.name.last}</h3>
+        <p>${user.location.city}, ${user.dob.date}</p>
+        <em>Membre depuis : ${user.registered.date} jours</em>
+      </div>  
+      `
+		)
+		.join(''); //join() pour enlever les virgules
+};
+
+//appeler la fonction
+displayUsers();
