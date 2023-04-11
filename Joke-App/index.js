be200531bec58cc731fd8480a6fd02174e2f7eca
Ht <htcvhs971@gmail.com>
2023-04-11 10:34:58 +0200

    Joke App Destructuring

--- Joke-App/index.js
+++ Joke-App/index.js
@@ -11,20 +11,22 @@ function getJoke() {
 	//2-coller le lien ds fetch et recupérer les données ,les enregistrer ds une variable data et les afficher ds la console
 	fetch('https://api.blablagues.net/?rub=blagues')
 		.then((response) => response.json())
-		.then((data) => {
+		//12bis-on peut aussi faire un destructuring avec data ou content ils disparaissent au lieu de data.data.content on aura :
+		.then(({ data }) => {
 			//3-j'affiche l'en tête de la blague ds la console (je px le faire pas à pas :data puis data.data...)
 			{
 				//11-on déclare une variable pour eviter dat.data.content
-				const joke = data.data.content;
+				//12-on peut aussi faire un destructuring avec content ou data ils disparaissent au lieu de data.data.content on aura :
+				const { content } = data;
 				//5-on log les données pour voir ce qu'on a (en direct)
 				//console.log(joke); //provisoire
 				//6-on affiche les données ds le DOM
-				headerJoke.textContent = joke.text_head;
+				headerJoke.textContent = content.text_head;
 				//!7-vu qu'il y a 3 types de réponse, on fait une ternaire pour afficher la bonne (boolean)
 				contentJoke.textContent =
 					/*si joke.text est différent de '' alors...
       car si on fait joke.text === true il ne fera pas la diff */
-					joke.text !== '' ? joke.text : joke.text_hidden;
+					content.text !== '' ? content.text : content.text_hidden;
 			}
 		});
 }
