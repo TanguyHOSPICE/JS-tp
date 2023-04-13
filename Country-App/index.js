f571241c739b95e9b364f610d947fc0f2280341d
Ht <htcvhs971@gmail.com>
2023-04-13 14:46:02 +0200

    Adjust flags sizes & add call fetchCountries on window load

--- Country-App/index.js
+++ Country-App/index.js
@@ -3,7 +3,7 @@
 const countriesContainer = document.querySelector('.countries-container');
 const result = document.querySelector('#result');
 // 3 - Passer les données à une variable
-let countries = [];
+let countriesData = [];
 
 // 4ter - fonction transforme 1ière lettre en majuscule
 const capitalizeFirstLetter = (string) => {
@@ -14,29 +14,35 @@ const capitalizeFirstLetter = (string) => {
 const fetchCountries = async () => {
 	await fetch('https://restcountries.com/v3.1/all')
 		.then((res) => res.json())
-		.then((data) => (countries = data[0])); //!provisoire
+		// 3bis - Récupérer les données de l'API ATTENTION: plusieurs lignes mettre des accolades
+		.then((data) => {
+			// 3bis - logguer les données de la variable data
+			//console.log(data); //!provisoire
+			// 3ter - passer les données à la variable countriesData
+			countriesData = data;
+			//3quater - afficher les données dans la console
+			console.log(countriesData); //!provisoire
+		});
 
-	// 3bis - logguer les données dans la variable countries
-	console.log(countries); //!provisoire
+	displayCountries();
 };
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 const displayCountries = () => {
 	// 4ter - afficher a l'ecran les cartes de chaque pays et boucler sur les données
-	result.innerHTML = countries
+	result.innerHTML = countriesData
 		.map((country) => {
 			return `
-          <li class="card">
-            <img src="${country.flags[0]}" alt="${country.name.common}" />
-            <h2>${capitalizeFirstLetter(country.name.common)}</h2>
-            <p><strong>Capital:</strong> ${country.capital}</p>
-            <p><strong>Population:</strong> ${country.population}</p>
-            <p>
-              <strong>Region:</strong> ${country.region} 
-              <br> 
-              <strong>Subregion:</strong> ${country.subregion}
-            </p>
-      
-          </li>
+        <li class="card">
+          <img src="${country.flags.svg}" alt=" drapeau ${country.translations.fra.common}" />
+          <h2>${capitalizeFirstLetter(country.translations.fra.common)}</h2>
+          <p><strong>Capital:</strong> ${country.capital}</p>
+          <p><strong>Population:</strong> ${country.population}</p>
+          <p>
+            <strong>Region:</strong> ${country.region} 
+            <br> 
+            <strong>Subregion:</strong> ${country.subregion}
+          </p>    
+        </li>
       `;
 		})
 		.join('');
@@ -50,6 +56,5 @@ const displayCountries = () => {
 // 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
 
 // ----------------- //
-//appel de la fonction
-fetchCountries();
-displayCountries();
+//appel de la fonction au chargement de la page
+window.addEventListener('load', fetchCountries);
